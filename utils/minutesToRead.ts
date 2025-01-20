93e2cedf0d95c9e4eab7e1dfec62bde76608fd20
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-20 14:39:22 -0300

    refactor: improve code formatting and consistency in date conversion and reading functions

--- utils/minutesToRead.ts
+++ utils/minutesToRead.ts
@@ -1,18 +1,17 @@
-export default function countMinutes  (value: string) {
+export default function countMinutes(value: string): string {
   value.replace(/(\r\n|\n|\r)/g, ' ').trim();
   const count = value.split(/\s+/g).length - 1;
   const minutes = count / 150;
   if (minutes < 1) {
     return '1 min';
   } if (minutes === 60) {
-    const minuto = `${minutes.toFixed(0)  } min`;
-    return minuto;
+    const minute = `${minutes.toFixed(0)} min`;
+    return minute;
   } if (minutes < 60) {
-    const minutos = `${minutes.toFixed(0)  } min`;
-    return minutos;
+    const minutesStr = `${minutes.toFixed(0)} min`;
+    return minutesStr;
   } if (minutes === 60) {
-    return '1 Hora';
+    return '1 Hour';
   }
-    return '+ de 1 Hora';
-
-};
+  return 'More than 1 Hour';
+}
